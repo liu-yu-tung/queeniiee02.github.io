b8e0064d45dfbe7cad323ebec2a317225964bd5d
liuyu <queeniiee02@gmail.com>
2022-10-04 01:08:59 +0800

    buggy hw2

--- README.md
+++ README.md
@@ -1,26 +1,27 @@
-# Web Programming HW#1
-**feature**
-1. support RWD 
-2. support icon-changing when user clickging
-3. include interactive effects 
-## implementaion
-1. Divide the page into three sections, and each sections include its small sections
-2. adding classes for the elements 
+# Web Programming HW#2
+
+## features
+
+1. support RWD
+2. avaible for custom number of people from 1 to 15
+3. the "你" user will not be removed
+4. the current time will update automatically 
+
+## implementation
+
+1. Divide the page into three sections, and each section include its small sections
+2. Detect if any remove button or pin button is triggered and update the view
+3. Create and remove instances via main.js
+
 ### main(left part)
-- setting the first user in this panel inside a container 
-- has a panel on the user icon supporting hover transition
+
+- Setting display to "none" when there is no pined person
+
 ### side(right part)
-- setting the other five users inside a container
-- the user panels change sizes and position according to the windows size similar to origin Google Meet
+
+- Append new person in this section when there is a new one need to be display here
+- Changing display when there is no pined person
 
 ### footer(bottom part)
-- divide footer into three part, left, middle and right
-1. left
-    - include information of the meeting
-2. middle 
-    - include six bottoms supporting icon-changing when user clickging
-    - bottoms have hover effects and hint box animation
-3. right
-    - include five bottoms supporting hover effects and hint box animation
-        
 
+- Mainly use the css setting from HW1
---
+++ main.js
@@ -0,0 +1,286 @@
+console.log(selection);
+function update_card_width() {
+    console.log("update card width, n: ", card.curr_num);
+
+    var remove_n_plus_1 = "p_" + (card.curr_num + 1);
+    var remove_n_minus_1 = "p_" + (card.curr_num - 1);
+    var add_n = "p_" + (card.curr_num);
+    var sid_card_container = document.getElementById("side_container");
+    sid_card_container.classList.remove(remove_n_minus_1);
+    sid_card_container.classList.remove(remove_n_plus_1);
+    sid_card_container.classList.add(add_n);
+    if(!card.pined_bool) {
+        sid_card_container.classList.add("adjust");
+    }
+    else {
+        sid_card_container.classList.remove("adjust");
+    }
+}
+class card {
+    constructor(pin_self, name, path) {
+        this.pin_self = pin_self;
+        this.name = name;
+        this.path = path;
+        let make_node = () => {
+            let node = document.createElement("div");
+            node.className = "card";
+            let remove_bottom = document.createElement("div");
+            remove_bottom.className = "remove_bottom screen_bottom";
+            remove_bottom.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="M4.5 17q-.625 0-1.062-.438Q3 16.125 3 15.5v-11q0-.625.438-1.062Q3.875 3 4.5 3H10v1.5H4.5v11H10V17Zm9-3.5-1.062-1.062 1.687-1.688H8v-1.5h6.125l-1.687-1.688L13.5 6.5 17 10Z"/></svg>';
+            //remove_bottom.setAttribute("type", "button");
+
+            let three_bottoms = document.createElement("div");
+            three_bottoms.className = "three_bottoms";
+
+            let three_bottoms_left = document.createElement("div");
+            three_bottoms_left.className = "tooltip left_bottom screen_bottom";
+            three_bottoms_left.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" ><rect id="backgroundrect" width="100%" height="100%" x="0" y="0" fill="none" stroke="none"/><g class="currentLayer" ><title>Layer 1</title><path d="m16 12 2 2v2h-5v6l-1 1-1-1v-6H6v-2l2-2V5H7V3h10v2h-1Z" id="svg_1" class=""/><path fill="none" stroke="#fff" stroke-opacity="1" stroke-width="2" stroke-dasharray="none" stroke-linejoin="round" stroke-linecap="butt" stroke-dashoffset="" fill-rule="nonzero" opacity="1" marker-start="" marker-mid="" marker-end="" d="M4.021065903957926,4.1949427251193345 L19.836733573662723,22.285713993362844 " id="svg_2" class=""/></g></svg>';
+
+            let three_bottoms_middle = document.createElement("div");
+            three_bottoms_middle.className = "tooltip middle_bottom screen_bottom";
+            three_bottoms_middle.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M3 11 V3 h8 v8 Z  m0 10 v-8 h8 v8 Z  m 10 -10 V3 h8 v8 Z  m0 10 v-8 h8 v8 Z  M5 9h 4V 5H 5Z m10 0 h4 V5 h-4 Z  m0 10 h4 v-4 h-4 Z  M5 19 h4 v-4 H5 Z  M15 9 Z  m0 6Zm-6 0Zm0-6Z"/></svg>';
+            
+            let three_bottoms_right = document.createElement("div");
+            three_bottoms_right.className = "tooltip right_bottom screen_bottom";
+            three_bottoms_right.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M3.4 22 2 20.6 8.6 14H4v-2h8v8h-2v-4.6ZM12 12V4h2v4.6L20.6 2 22 3.4 15.4 10H20v2Z"/></svg>';
+
+            three_bottoms.appendChild(three_bottoms_left);            
+            three_bottoms.appendChild(three_bottoms_middle);            
+            three_bottoms.appendChild(three_bottoms_right);            
+
+            let block_b1 = document.createElement("div");
+            block_b1.className = "block b1";
+            let mute = document.createElement("div");
+            mute.className = "mute";
+            mute.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="M14.604 12.479 13.5 11.375q.229-.437.365-.906Q14 10 14 9.5h1.5q0 .792-.229 1.552t-.667 1.427Zm-2.229-2.229L7.5 5.375V4.5q0-1.042.729-1.771Q8.958 2 10 2q1.042 0 1.771.729.729.729.729 1.771v5q0 .188-.031.375t-.094.375ZM9.25 17v-2.062q-2.062-.25-3.406-1.803Q4.5 11.583 4.5 9.5H6q0 1.667 1.167 2.833Q8.333 13.5 10 13.5q.708 0 1.344-.271.635-.271 1.177-.729l1.083 1.083q-.604.521-1.333.886-.729.364-1.521.469V17ZM16 18.125 1.875 3.979l1.063-1.062 14.124 14.145Z"/></svg>';
+            if (name == "you") {
+                remove_bottom.id = "main_remove";
+                if (!pin_self) {
+                    remove_bottom.classList.add("you_remove");
+                }
+            }
+            block_b1.append(remove_bottom);
+            block_b1.append(mute);
+
+            let block_b2 = document.createElement("div");
+            block_b2.className = "block b2";
+            let circle = document.createElement("div");
+            circle.className = "circle";
+            let text_box = document.createElement("div");
+            text_box.className = "text_box";
+            let content = document.createElement("div");
+            content.className = "content";
+            if (name == "you") {
+                content.innerHTML = "你";
+            }
+            else {
+                content.innerHTML = name;
+            }
+            text_box.appendChild(content);
+            circle.appendChild(text_box);
+            block_b2.appendChild(circle);
+
+            
+            let block_b3 = document.createElement("div");
+            block_b3.className = "block b3";
+            let status = document.createElement("div");
+            status.className = "status";
+            let name_bar = document.createElement("div");
+            name_bar.className = "name";
+            name_bar.innerHTML = name;
+            if (name != "you") {
+                status.appendChild(name_bar);
+            }
+            block_b3.appendChild(status);
+
+            node.appendChild(three_bottoms);
+            node.appendChild(block_b1);
+            node.appendChild(block_b2);
+            node.appendChild(block_b3);
+
+            if (pin_self) {
+                var m = document.getElementById("main_container");
+                content.classList.add("main_content");
+                m.appendChild(node);
+            }
+            else {
+                var s = document.getElementById("side_container");
+                s.appendChild(node);
+            }
+            card.count++;
+            card.curr_num++;
+            if (this.name == "you") {
+                card.pined_bool = true;
+            }
+
+            remove_bottom.onclick = () => {
+                if (this.name == MAIN.name) {
+                    MAIN.name = "empty";
+                    MAIN.path = "empty";
+                    card.pined_Person_name = "";
+                    card.pined_bool = false;
+                }
+                else {
+                    //this.clean();
+                    node.remove();
+                }
+                card.curr_num--;
+                if_pined();
+            };
+
+            three_bottoms_left.onclick = () => {
+                if (card.pined_bool) {
+                    if(MAIN.name == this.name) {
+                        if (card.curr_num > 1) {
+                            var main_txt = document.getElementsByClassName("main_content");
+                            main_txt.innerHTML = "empty";
+                            this.push_to_side();
+                            card.curr_num--;
+                        }
+                    }
+                    else {
+                        this.swap_pined();
+                        //this.clean();
+                        node.remove();
+                        card.curr_num--;
+                        card.pined_bool = true;
+                    }
+                }
+                else {
+                    this.push_to_main();
+                    node.remove();
+                    card.curr_num--;
+                    card.pined_bool = true;
+                }
+                if_pined();
+            };
+        };
+        this.node = make_node();
+
+        console.log("construct ", this.name);
+    }
+    swap_pined = () => {
+        if (this.name != MAIN.name) {
+            var tmp_name = MAIN.name;
+            var tmp_path = MAIN.path;
+            MAIN.name = this.name;
+            MAIN.path = this.path;
+            var old_main_to_side = new card(false, tmp_name, tmp_path);
+            let main_txt = document.getElementsByClassName("main_content");
+            main_txt[0].innerHTML = MAIN.name; 
+            card.pined_bool = true;
+            card.curr_num--;
+        }
+        if_pined();
+    }
+    push_to_main = () => {
+        let main_txt = document.getElementsByClassName("main_content");
+        main_txt[0].innerHTML = this.name; 
+        card.pined_bool = true;
+        var tmp_name = this.name;
+        var tmp_path = this.path;
+        //this.clean();
+        MAIN.name = tmp_name;
+        MAIN.path = tmp_path;
+        card.pined_Person_name = this.name;
+        card.pined_Person_path = this.path;
+        if_pined();
+    }
+
+    push_to_side = () => {
+        let main_txt = document.getElementsByClassName("main_content");
+        var push = new card(false, MAIN.name, MAIN.path);
+        card.pined_bool = false;
+        MAIN.name = "";
+        MAIN.path = "";
+        card.pined_Person_name = "";
+        card.pined_Person_path = "";
+        if_pined();
+    }
+
+    clean = () => {
+        if (this.name == MAIN.name) {
+            card.pined_bool = false;
+
+            //card.pined_Person_name = "";
+            //card.pined_Person_path = "";
+        }
+        else {
+        }
+        card.curr_num--;
+            if_pined();
+    };
+
+    static pined_Person_name = "";
+    static pined_Person_path = "";
+    static count = 0;
+    static curr_num = 0;
+    static pined_bool = true;
+}
+
+function createNewPeople() {
+    if (card.curr_num < 15) {
+        var newPeople = new card(false, card.count, "./");
+    }
+    else {
+        console.log("already 15");
+    }
+}
+
+function if_pined() {
+    let m = document.getElementById("main");
+    let s = document.getElementById("side");
+    if(card.pined_bool) {
+        m.style.display = "block";
+        m.style.width = "70%";
+        s.style.display = "sticky";
+        s.style.width = "30%";
+    }
+    else {
+        m.style.display = "none";
+        s.style.width = "100%";
+    }
+    if (MAIN.name == "you") {
+        var you_none = document.getElementById("main_remove");
+        you_none.style.display = "none"; 
+    }
+    else {
+        var main_visible = document.getElementById("main_remove");
+        main_visible.classList.remove("you_remove");
+        main_visible.style.display = "block";
+        var you_none = document.getElementsByClassName("you_remove");
+        if (you_none[0]) {
+            you_none[0].style.display = "none"; 
+        }
+    }
+    update_card_width();
+}
+
+//start
+console.log("Hello World");
+var p = document.getElementById("pppp");
+
+if (p) {
+    p.onclick = function() {
+        createNewPeople();    
+        if_pined();
+    }
+}
+else {
+}
+update_time();
+function update_time() {
+    let time = new Date();
+    document.getElementById("curr_time").innerHTML = time.toLocaleTimeString('em-US', {hour:'numeric', minute:'numeric', hour12: true});
+};
+setInterval(update_time, 1000);
+
+MAIN = new card(true, "you", "./");
+if_pined();
+//createYou();
+for (var i = 1; i<selection; i++) {
+    var pp = new card(false, card.curr_num, "./");
+}
+if_pined();
+
+//end
